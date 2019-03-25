a8d2562ee1eae4a1503505ec6c8f5eeba348c050
Werner Barnard <wbarnard81@gmail.com>
2019-03-25 15:22:49 +0200

    Trying to resolve the error

--- client/src/store/authentication.js
+++ client/src/store/authentication.js
@@ -3,28 +3,28 @@ import HTTP from '../http';
 export default {
   namespaced: true,
   state: {
-      registerEmail: null,
-      registerPassword: null,
-      registerError: null,
-      token: null,
+    registerEmail: null,
+    registerPassword: null,
+    registerError: null,
+    token: null,
   },
   actions: {
-    register({commit, state}) {
-      return HTTP().post('http://localhost:3333/api/auth/register', {
+    register({ commit, state }) {
+      return HTTP().post('/auth/register', {
         email: state.registerEmail,
         password: state.registerPassword,
       });
-    }
+    },
   },
   mutations: {
-      setToken(state, token) {
-          state.token = token;
-      },
-      setRegisterEmail(state, email) {
-          state.registerEmail = email;
-      },
-      setRegisterPassword(state, password) {
-          state.registerPassword = password;
-      },
+    setToken(state, token) {
+      state.token = token;
+    },
+    setRegisterEmail(state, email) {
+      state.registerEmail = email;
+    },
+    setRegisterPassword(state, password) {
+      state.registerPassword = password;
+    },
   },
 };
